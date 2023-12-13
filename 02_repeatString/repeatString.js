8be09e8688dcd9194ec20f0d7279d91709207154
Falk B. <86295521+falk234@users.noreply.github.com>
2023-12-13 18:52:21 +0100

    Solve exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    let returnString='';
+    if (number>=0){  
+        for (i=0;i<number;i++){
+            returnString+=string;
+        }
+    }else{
+        returnString="ERROR"
+    }
+    return returnString;
 };
 
 // Do not edit below this line
