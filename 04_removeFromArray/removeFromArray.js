77670bcde06c0b7f636f0120f6a6544eb8290186
Falk B. <86295521+falk234@users.noreply.github.com>
2023-12-15 13:20:25 +0100

    Remove obsolete code for better readabilit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,8 @@
-// let array =[1,2,3,4,5,6];
-
 const removeFromArray = function(...input){
     let toRemove=[];
     let array=[];
     for (let i=0;i<input.length;i++){
-        if (i==0){
-            array=input[i];
-        }else{
-            toRemove.push(input[i]);
-        }
-    }     
-    // console.log("Array: " + array);
-    // console.log("toRemove: " + toRemove);
+        (i==0)?array=input[i]:toRemove.push(input[i]);
 
     for (let i=0;i<array.length;i++){
         for (let j=0;j<toRemove.length;j++){
@@ -20,13 +11,8 @@ const removeFromArray = function(...input){
             }
         }
     }
-
-    // console.log("Neues Array: " + array);
-
     return array;
 }
-
-// removeFromArray(array,3,4,5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
