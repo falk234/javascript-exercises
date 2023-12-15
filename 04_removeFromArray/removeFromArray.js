8ba2b4958f529fec7bd399b04815be547d6b8a14
Falk B. <86295521+falk234@users.noreply.github.com>
2023-12-15 13:59:59 +0100

    Add mark to consider solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,4 @@
 const removeFromArray = function(array,...toRemove){
-    // let toRemove=[];
-    // let array=[];
-    // for (let i=0;i<input.length;i++){
-    //     (i==0)?array=input[i]:toRemove.push(input[i]);
-    // }
     for (let i=0;i<array.length;i++){
         for (let j=0;j<toRemove.length;j++){
             if (array[i]===toRemove[j]){
