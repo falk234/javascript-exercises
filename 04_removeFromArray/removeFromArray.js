da59abd6f92d5b26e12ab8bc41618489820a96ec
Falk B. <86295521+falk234@users.noreply.github.com>
2023-12-15 12:19:45 +0100

    Solve excercie, pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
+// let array =[1,2,3,4,5,6];
 
-};
+const removeFromArray = function(...input){
+    let toRemove=[];
+    let array=[];
+    for (let i=0;i<input.length;i++){
+        if (i==0){
+            array=input[i];
+        }else{
+            toRemove.push(input[i]);
+        }
+    }     
+    // console.log("Array: " + array);
+    // console.log("toRemove: " + toRemove);
+
+    for (let i=0;i<array.length;i++){
+        for (let j=0;j<toRemove.length;j++){
+            if (array[i]===toRemove[j]){
+                array.splice(i,1);
+            }
+        }
+    }
+
+    // console.log("Neues Array: " + array);
+
+    return array;
+}
+
+// removeFromArray(array,3,4,5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
