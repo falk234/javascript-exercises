afc871c3334da39e5caa36520ae3a129ec68e5e8
Falk B. <86295521+falk234@users.noreply.github.com>
2023-12-15 13:49:00 +0100

    Remove obsolete if-condition

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,9 @@
-const removeFromArray = function(...input){
-    let toRemove=[];
-    let array=[];
-    for (let i=0;i<input.length;i++){
-        (i==0)?array=input[i]:toRemove.push(input[i]);
-    }
-
+const removeFromArray = function(array,...toRemove){
+    // let toRemove=[];
+    // let array=[];
+    // for (let i=0;i<input.length;i++){
+    //     (i==0)?array=input[i]:toRemove.push(input[i]);
+    // }
     for (let i=0;i<array.length;i++){
         for (let j=0;j<toRemove.length;j++){
             if (array[i]===toRemove[j]){
