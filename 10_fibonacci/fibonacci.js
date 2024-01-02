bf6b7adc2fedfd0ab379d00ca8c0c582749adcc3
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-02 18:12:55 +0100

    Solve excercise, pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fiboArray = [1,1];
+    if (num ==0) return 0;
+        else if (num<0){
+            return "OOPS";}
+    for(let i=2; i<=num-1; i++){
+        fiboArray.push(+fiboArray[i-2] + +fiboArray[i-1]);
+    }
+
+    return fiboArray[--num];
 
 };
+
+// Fibonacci-Sequenz: "1, 1, 2, 3, 5, 8";
 
 // Do not edit below this line
 module.exports = fibonacci;
