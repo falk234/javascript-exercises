cc8d9d62c3dbe29a14faed0eb7716bf9fa50510c
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-02 17:29:03 +0100

    Fix function, so only numbers and characters are considered

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,24 +1,18 @@
 // Version without word_splitting
 const palindromes = function (myString) {
-//     const filteredString=[];
-//     const reversedFilteredString=[];
-//     filteredString = myString.split('').filter((character) => character.toLowerCase() != character.toLowerCase()).join('');
-//     reversedFilteredString = filteredString.split('').reverse().join('');
-//     return reversedFilteredString == filteredString;
-// };
 
-// Long Version
 // Transform whole sentence to character array
     const characterArray=myString.split("");
 // Remove  all non-letters from character array
-    const filteredCharacterArray = characterArray.filter((character) => character.toLowerCase() != character.toUpperCase());
+// => create new array if symbol is a number or a real character (upper and lowercase is different)
+    const filteredCharacterArray = characterArray.filter( (char) => /^[0-9]*$/.test(char) || char.toLowerCase() != char.toUpperCase());
 // Join array back to string
     let filteredString = filteredCharacterArray.join('');
 // reverse string
     let reversedFilteredString = filteredCharacterArray.reverse().join('');
 // return result of comparison
     return (filteredString.toLowerCase()===reversedFilteredString.toLowerCase());
-    };
+};
 
 
 // Do not edit below this line
