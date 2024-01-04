96b2d0b759e01a4c734c93475e2f9c602554366d
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-04 17:08:51 +0100

    Solve exercise, pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((previous_person, current_person) => {
+        let current_age;
+        if (current_person.yearOfDeath == undefined){
+            current_age = new Date().getFullYear() - current_person.yearOfBirth;
+        }
+        else current_age=current_person.yearOfDeath-current_person.yearOfBirth;
 
+        let previous_age;
+        if (previous_person.yearOfDeath == undefined){
+            previous_age = new Date().getFullYear() - previous_person.yearOfBirth;
+        }
+        else previous_age=previous_person.yearOfDeath-previous_person.yearOfBirth;
+
+        if (previous_age > current_age) return previous_person;
+        else return current_person;
+    } );
 };
 
 // Do not edit below this line
